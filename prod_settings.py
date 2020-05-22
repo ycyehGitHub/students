1d0785f68ca98f71b747020a910612083ce6f500
ycyehGitHub <65749243+ycyehGitHub@users.noreply.github.com>
2020-05-22 11:23:56 +0800

    Add files via upload

---
+++ prod_settings.py
@@ -0,0 +1,16 @@
+from .settings import *   # 含入原本的settings.py所有設定
+
+import dj_database_url
+# heroku使用的資料庫為PostgreSQL，所以要修改資料庫設定
+DATABASES = {
+    'default': dj_database_url.config(),
+}
+
+STATIC_ROOT = 'static' # 設定網站正式上線時靜態檔案目錄位置
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# 設定HTTP連線方式
+ALLOWED_HOSTS = ['*'] # 讓所有的網域都能瀏覽本網站
+DEBUG = False # 關閉除錯模式
+
+
+
